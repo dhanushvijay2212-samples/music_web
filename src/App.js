75a80085a9723a604d3eb40bd90c13f454c8b838
dhanushvijay <dhanushvijay2212@gmail.com>
2026-02-15 21:12:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 } from "@mui/material";
 import { SkipNext, SkipPrevious, PlayArrow, Pause } from "@mui/icons-material";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://192.168.1.9:8080";
 
 function App() {
   const [songs, setSongs] = useState([]);
